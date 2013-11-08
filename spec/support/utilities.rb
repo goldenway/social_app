bed967b14575c790bbc57d39051cec694fc46685
Igor Goldenway <goldenway777@gmail.com>
2013-11-08 22:20:30 +0200

    Make a basic User model (including secure passwords)

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,2 @@
 # також є такий хелпер в app/helpers/application_helper.rb
-
-def full_title(page_title)
-	base_title = "Social App"
-	if page_title.empty?
-		base_title
-	else
-		"#{base_title} | #{page_title}"
-	end
-end
+include ApplicationHelper
