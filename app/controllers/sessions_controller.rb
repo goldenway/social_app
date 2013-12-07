b86d3a8e191fc5a9af1b55b6f2d26293ae13a5af
Igor Goldenway <goldenway777@gmail.com>
2013-12-07 22:25:20 +0200

    Finish user edit, update, index and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,14 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by_email(params[:session][:email].downcase)
+		# when using "form_for" in app/views/sessions/new.html.erb use
+		# params[:session][:email] and params[:session][:password]
+		
+		user = User.find_by_email(params[:email].downcase)
 
-		if user && user.authenticate(params[:session][:password])
+		if user && user.authenticate(params[:password])
 			sign_in user
-			redirect_to user
+			redirect_back_or user   # friendly forwarding or redirect to user page
 		else
 			flash.now[:error] = 'Invalid email/password combination'
 			render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+	before_filter :unsigned_user,  only: [:new, :create]
+	before_filter :correct_user,   only: [:edit, :update]
+	before_filter :admin_user,     only: :destroy
+
+	def index
+		@users = User.paginate(page: params[:page], per_page: 10)
+		# when changing :per_page count, change it in user_pages_spec.rb "pagination"
+	end
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -20,4 +30,52 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
+
+	def edit
+	end
+
+	def update
+		if @user.update_attributes(params[:user])
+			flash[:success] = "Profile updated"
+			sign_in @user
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User destroyed."
+		redirect_to users_url
+	end
+
+	private
+
+		def signed_in_user
+			unless signed_in?
+				store_location
+
+				redirect_to signin_url, notice: "Please sign in."
+				# the same as
+				# flash[:notice] = "Please sign in."
+				# redirect_to signin_url
+				# Аналогичная конструкция работает для ключа :error, но не для :success
+			end
+		end
+
+		def unsigned_user
+			if signed_in?
+				redirect_to root_path, notice: "You already have an account"
+			end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
+
+		def admin_user
+			redirect_to(root_path) unless current_user.admin?
+		end
 end
